2ccc53afb74bda7edcd7c7e932767cd5113b0d9a
Shivanand Malkapuri <shivanandmalkapuri@gmail.com>
2026-08-14 13:35:43 +0530

    program1 updates of daily routine

--- src/program1.java
+++ src/program1.java
@@ -3,6 +3,64 @@ public class program1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world i am learning java");
+		
+		var alaram = true;
+		var bath = true;
+		var breakfast = true;
+		var learn = true;
+		var homework = true;
+		var hungry = true;
+		var sleep = true;
+		
+		
+		if (alaram) {
+			System.out.println("Wake- up");
+		}else {
+			System.out.println("Not waking up");
+		}
+		System.out.println("Go to bathroom");
+		
+		if (bath) {
+			System.out.println("Do not take a Bath");
+		} else {
+			System.out.println("Take a bath");
+		}
+		System.out.println("Go to Break fast");
+		
+		if (breakfast) {
+			System.out.println("Eat  breakFast");
+		} else {
+			System.out.println("Do not Eat Break fast");
+		}
+		System.out.println("Go to Learn");
+		
+		if (learn) {
+			System.out.println("Continue to learn");
+		} else {
+			System.out.println("dont learn");
+		}
+		System.out.println("Go to Home work");
+		
+		if (homework) {
+			System.out.println("Do HomeWork");
+		} else {
+			System.out.println("Not waking up");
+		}
+		System.out.println("Do not do home work");
+		
+		if (hungry) {
+			System.out.println("Eat some thing good");
+		} else {
+			System.out.println("dont eat anything");
+		}
+		System.out.println("Go to sleep");
+		
+		if (sleep) {
+			System.out.println("Sleep");
+		} else {
+			System.out.println("Do not sleep use whatsapp");
+		}
+		
 
 	}
 
